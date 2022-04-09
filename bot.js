67907ad0c59fcdad4d340699787904a09ce9e3bc
Jrockbritto <jrockbritto@gmail.com>
2022-04-08 23:12:06 -0300

    adicionando tempo de atualização, chamado everyone entre outros

--- bot.js
+++ bot.js
@@ -11,18 +11,19 @@ let old_players = null;
 let old_status = null;
 let old_time = null;
 let notifyed = null;
+let old_formattedTime = null;
 
 dotenv.config();
 
 const track = (client, message, id) => {
   axios.get('https://api.trackyserver.com/widget/index.php?id=' + id)
   .then((response) => {
-    console.log("updating...")
+    console.log("updating...");
     if(response.data){
         updatePresence(client, response, id, message);
     } else {
-      message.reply("Não foi possivel encontrar o servidor...")
-      clearInterval(interval)
+      message.reply("Não foi possivel encontrar o servidor...");
+      clearInterval(interval);
     }
   })
 }
@@ -31,7 +32,7 @@ const setNick = (client,message,id) => {
   let name = null;
   axios.get('https://api.trackyserver.com/widget/index.php?id=' + id)
   .then((response) => {
-    console.log('updateNick...')
+    console.log('updateNick...');
     if(response.data.name.length > 32) {
       name  = response.data.name.substring(0,29) + '...';
     }
@@ -44,53 +45,84 @@ const setNick = (client,message,id) => {
 }
 
 const updatePresence = (client, response, id, message) => {
-  try{
-    
-  let status = null
+  try {
+      if(response.data.playerscount != 'offline') {
+        
+        let status = null;
 
-  let players = response.data.playerscount
+        let players = response.data.playerscount;
 
-  let [ time ] = response.data.resources.match(/(\d\d?:\d\d)/g)
+        let [ time ] = response.data.resources.match(/(\d\d?:\d\d)/g);
 
-  let [now, maxPlayers] = players.split('/')
-  
-  let playerRate = parseFloat(parseInt(now)/parseInt(maxPlayers)).toFixed(1)
+        let unix_timestamp = response.data.date
 
-  if(playerRate <= 0.3){
-    status = 'online'
-    notifyed = false;
-  }
-  else if(playerRate < 0.7){
-    status = 'idle';
-    notifyed = false;
-  }
-  else{
-    status = 'dnd';
-    if(!notifyed) {
-      const populationEmbed = new Discord.MessageEmbed()
-        .setColor('#0ED611')
-        .setTitle('High Population!  :people_wrestling: ')
-        .setDescription(`>>> O servidor está com ${playerRate*100}% da sua capacidade!`)
-        .setThumbnail('https://fontmeme.com/images/Dayz-Game.jpg')
-        .addField('Mais informações (More info)', 'https://www.trackyserver.com/server/'+ id)
-        .setTimestamp()
+        var date = new Date(unix_timestamp);
+        // Hours part from the timestamp
+        var hours = date.getHours();
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+        // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
 
-        message.reply({ embeds: [populationEmbed] }).then(msg => {
-          setTimeout(() => msg.delete(), 60000)
-        })
-        .catch(console.error);
-        notifyed = true;
+        // Will display time in 10:30:23 format
+        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+        let [now, maxPlayers] = players.split('/');
+        
+        let playerRate = parseFloat(parseInt(now)/parseInt(maxPlayers)).toFixed(1);
+
+        if(playerRate <= 0.3){
+          status = 'online';
+          notifyed = false;
+        }
+        else if(playerRate < 0.7){
+          status = 'idle';
+          notifyed = false;
+        }
+        else{
+          status = 'dnd';
+          if(!notifyed) {
+            const populationEmbed = new Discord.MessageEmbed()
+              .setColor('#0ED611')
+              .setTitle('High Population!  :people_wrestling: ')
+              .setDescription(`>>> O servidor está com ${playerRate*100}% da sua capacidade! `)
+              .setThumbnail('https://fontmeme.com/images/Dayz-Game.jpg')
+              .addField('Mais informações (More info)' , 'https://www.trackyserver.com/server/'+ id)
+              .setTimestamp()
+
+              message.channel.send({content:'@everyone', embeds: [populationEmbed] }).then(msg => {
+                setTimeout(() => msg.delete(), 60000)
+              })
+              .catch(console.error);
+              notifyed = true;
+          }
+        }
+        console.log({ maxPlayers, now, playerRate, status });
+        console.log('updatePresence...');
+        console.log({old_status,status,old_players,players,old_time,time,notifyed,old_formattedTime,formattedTime,condition:(old_players != players || old_status != status || old_time != time || old_formattedTime != formattedTime)})
+        if(old_players != players || old_status != status || old_time != time || old_formattedTime != formattedTime){
+          client.user.setPresence({ activities: [{ name: `${players} ${time}\nupdated at: ${formattedTime}`, type: 'PLAYING' }], status: status });
+          old_players = players;
+          old_status = status;
+          old_time = time;
+          old_formattedTime = formattedTime;
+        }
+    } else {
+              
+      let status = 'dnd';
+
+      let players = 'offline';
+
+      let time = 'no-data'
+
+      console.log({old_status,status,old_players,players,old_time,time,condition:(old_players != players || old_status != status || old_time != time)})
+      if(old_players != players || old_status != status){
+        client.user.setPresence({ activities: [{ name: `${players} ${time}`, type: 'PLAYING' }], status: status });
+        old_players = players;
+        old_status = status;
+        old_time = time;
+      }
     }
-  }
-  console.log({ maxPlayers, now, playerRate, status });
-  console.log('updatePresence...');
-  console.log({old_status,status,old_players,players,old_time,time,notifyed,condition:(old_players != players || old_status != status || old_time != time)})
-  if(old_players != players || old_status != status){
-    client.user.setPresence({ activities: [{ name: `${players} ${time}`, type: 'PLAYING' }], status: status });
-    old_players = players;
-    old_status = status;
-    old_time = time;
-  }
   }
   catch(e) {
     console.error(e);
@@ -180,7 +212,14 @@ const client = new Client({
             break;
 
           case 'flamengo':
-            message.reply("é é o Ronaldinho Gaucho ronal ronaldinho Gaucho é é Thiago Neves!! To sem freio to sem freio é o bonde do mengão sem freio!!!!").then(msg => {
+            message.reply("É é o Ronaldinho Gaucho ronal ronaldinho Gaucho é é Thiago Neves!! To sem freio to sem freio é o bonde do mengão sem freio!!!!").then(msg => {
+              setTimeout(() => msg.delete(), 10000)
+            })
+            .catch(console.error);
+            break;
+
+          case 'vasco':
+            message.reply("É o vaxco porra!!!!!").then(msg => {
               setTimeout(() => msg.delete(), 10000)
             })
             .catch(console.error);
@@ -211,36 +250,82 @@ const client = new Client({
             axios.get('https://api.trackyserver.com/widget/index.php?id=' + args[0])
             .then((response) => {
               try{
-                if(response.data){
-                  let [ time ] = response.data.resources.match(/(\d\d?:\d\d)/g)
-                  const StatusEmbed = new Discord.MessageEmbed()
-                  .setColor('#0ED611')
-                  .setTitle('Status!  :bar_chart:')
-                  .setDescription(`>>> ${response.data.name}`)
-                  .setThumbnail('https://fontmeme.com/images/Dayz-Game.jpg')
-                  .addFields(
-                    { name: 'Ip', value: response.data.ip, inline: true},
-                    { name: 'Players', value: response.data.playerscount, inline: true },
-                    { name: 'Mapa (Map)', value: response.data.map, inline: true })
-                  .addFields(
-                    { name: 'Horario (Time)', value: time, inline: true},
-                    { name: 'País (Country)', value: response.data.country, inline: true },
-                    { name: 'Versão (Version)', value: response.data.version, inline: true },
-                  )
-                  .addField('Mais informações (More info)', 'https://www.trackyserver.com/server/'+args[0])
-                  .setTimestamp()
-  
-                  message.reply({ embeds: [StatusEmbed] }).then(msg => {
+                if(response.data.playerscount != 'offline') {
+                  if(response.data){
+                    let [ time ] = response.data.resources.match(/(\d\d?:\d\d)/g)
+
+                    let unix_timestamp = response.data.date
+
+                    var date = new Date(unix_timestamp);
+                    // Hours part from the timestamp
+                    var hours = date.getHours();
+                    // Minutes part from the timestamp
+                    var minutes = "0" + date.getMinutes();
+                    // Seconds part from the timestamp
+                    var seconds = "0" + date.getSeconds();
+
+                    // Will display time in 10:30:23 format
+                    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+                    const StatusEmbed = new Discord.MessageEmbed()
+                    .setColor('#0ED611')
+                    .setTitle('Status!  :bar_chart:')
+                    .setDescription(`>>> ${response.data.name}`)
+                    .setThumbnail('https://fontmeme.com/images/Dayz-Game.jpg')
+                    .addFields(
+                      { name: 'Ip', value: response.data.ip, inline: true},
+                      { name: 'Players', value: response.data.playerscount, inline: true },
+                      { name: 'Mapa (Map)', value: response.data.map, inline: true })
+                    .addFields(
+                      { name: 'Horario (Time)', value: time, inline: true},
+                      { name: 'País (Country)', value: response.data.country, inline: true },
+                      { name: 'Versão (Version)', value: response.data.version, inline: true },
+                    )
+                    .addFields(
+                      { name: 'Mais informações (More info)', value: 'https://www.trackyserver.com/server/'+args[0], inline: true},
+                      { name: 'Atualizado em (updated at)', value: formattedTime, inline: true },
+                    )
+                    .setTimestamp()
+    
+                    message.reply({ embeds: [StatusEmbed] }).then(msg => {
+                      setTimeout(() => msg.delete(), 30000)
+                    })
+                    .catch(console.error);
+                  } else {
+                  message.reply("Não foi possivel encontrar o servidor...").then(msg => {
                     setTimeout(() => msg.delete(), 20000)
                   })
                   .catch(console.error);
+                  } 
                 } else {
-                message.reply("Não foi possivel encontrar o servidor...").then(msg => {
-                  setTimeout(() => msg.delete(), 20000)
-                })
-                .catch(console.error);
-                } 
-              }catch{
+                    let [ time ] = 'offline'
+                    const StatusEmbed = new Discord.MessageEmbed()
+                    .setColor('#0ED611')
+                    .setTitle('Status!  :bar_chart:')
+                    .setDescription(`>>> Offline`)
+                    .setThumbnail('https://fontmeme.com/images/Dayz-Game.jpg')
+                    .addFields(
+                      { name: 'Ip', value: 'offline', inline: true},
+                      { name: 'Players', value: 'offline', inline: true },
+                      { name: 'Mapa (Map)', value: 'offline', inline: true })
+                    .addFields(
+                      { name: 'Horario (Time)', value: time, inline: true},
+                      { name: 'País (Country)', value: 'offline', inline: true },
+                      { name: 'Versão (Version)', value: 'offline', inline: true },
+                    )
+                    .addFields(
+                      { name: 'Mais informações (More info)', value: 'https://www.trackyserver.com/server/'+args[0], inline: true},
+                      { name: 'Atualizado em (updated at)', value: 'offline', inline: true },
+                    )
+                    .setTimestamp()
+    
+                    message.reply({ embeds: [StatusEmbed] }).then(msg => {
+                      setTimeout(() => msg.delete(), 20000)
+                    })
+                    .catch(console.error);
+                  }
+              }catch (e){
+                console.error(e)
                 message.reply('Use !status <ServerID> : para mostrar o status do servidor.').then(msg => {
                   setTimeout(() => msg.delete(), 20000)
                 })
